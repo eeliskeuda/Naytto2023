6418f152a5b34946c91890112c2907a3376e5283
eeliskeuda <99178265+eeliskeuda@users.noreply.github.com>
2023-05-29 15:29:45 +0300

    Add files via upload

---
+++ Naytto/Naytto/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace Naytto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PoistaButton = new System.Windows.Forms.Button();
+            this.MuokkaaButton = new System.Windows.Forms.Button();
+            this.HakuTextBox = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PostinumeroTextBox = new System.Windows.Forms.TextBox();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.YNimiTextBox = new System.Windows.Forms.TextBox();
+            this.YSpostiTextBox = new System.Windows.Forms.TextBox();
+            this.YPuhelinTextBox = new System.Windows.Forms.TextBox();
+            this.YYritysTextBox = new System.Windows.Forms.TextBox();
+            this.YRooliTextBox = new System.Windows.Forms.TextBox();
+            this.YLuoButton = new System.Windows.Forms.Button();
+            this.ALuoButton = new System.Windows.Forms.Button();
+            this.YtunnusTextBox = new System.Windows.Forms.TextBox();
+            this.KatuosoiteTextBox = new System.Windows.Forms.TextBox();
+            this.ToimialaTextBox = new System.Windows.Forms.TextBox();
+            this.YritysnimiTextBox = new System.Windows.Forms.TextBox();
+            this.KaupunkiTextBox = new System.Windows.Forms.TextBox();
+            this.APuhelinTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(53)))), ((int)(((byte)(76)))));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.PoistaButton);
+            this.panel1.Controls.Add(this.MuokkaaButton);
+            this.panel1.Controls.Add(this.HakuTextBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(984, 47);
+            this.panel1.TabIndex = 0;
+            // 
+            // PoistaButton
+            // 
+            this.PoistaButton.Location = new System.Drawing.Point(814, 11);
+            this.PoistaButton.Name = "PoistaButton";
+            this.PoistaButton.Size = new System.Drawing.Size(102, 27);
+            this.PoistaButton.TabIndex = 3;
+            this.PoistaButton.Text = "Poista";
+            this.PoistaButton.UseVisualStyleBackColor = true;
+            // 
+            // MuokkaaButton
+            // 
+            this.MuokkaaButton.Location = new System.Drawing.Point(679, 12);
+            this.MuokkaaButton.Name = "MuokkaaButton";
+            this.MuokkaaButton.Size = new System.Drawing.Size(102, 27);
+            this.MuokkaaButton.TabIndex = 2;
+            this.MuokkaaButton.Text = "Muokkaa";
+            this.MuokkaaButton.UseVisualStyleBackColor = true;
+            // 
+            // HakuTextBox
+            // 
+            this.HakuTextBox.Location = new System.Drawing.Point(12, 12);
+            this.HakuTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.HakuTextBox.MaxLength = 50;
+            this.HakuTextBox.Name = "HakuTextBox";
+            this.HakuTextBox.Size = new System.Drawing.Size(636, 26);
+            this.HakuTextBox.TabIndex = 0;
+            this.HakuTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(63)))), ((int)(((byte)(86)))));
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.DataGridView);
+            this.panel2.Location = new System.Drawing.Point(0, 47);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(984, 614);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(63)))), ((int)(((byte)(86)))));
+            this.panel3.Controls.Add(this.APuhelinTextBox);
+            this.panel3.Controls.Add(this.KaupunkiTextBox);
+            this.panel3.Controls.Add(this.YritysnimiTextBox);
+            this.panel3.Controls.Add(this.ToimialaTextBox);
+            this.panel3.Controls.Add(this.KatuosoiteTextBox);
+            this.panel3.Controls.Add(this.YtunnusTextBox);
+            this.panel3.Controls.Add(this.ALuoButton);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.PostinumeroTextBox);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(480, 161);
+            this.panel3.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(63)))), ((int)(((byte)(86)))));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(192, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Asiakas";
+            // 
+            // PostinumeroTextBox
+            // 
+            this.PostinumeroTextBox.Location = new System.Drawing.Point(234, 33);
+            this.PostinumeroTextBox.Name = "PostinumeroTextBox";
+            this.PostinumeroTextBox.Size = new System.Drawing.Size(216, 26);
+            this.PostinumeroTextBox.TabIndex = 3;
+            this.PostinumeroTextBox.Text = "Postinumero";
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(78)))), ((int)(((byte)(101)))));
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(0, 178);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.RowTemplate.Height = 24;
+            this.DataGridView.Size = new System.Drawing.Size(984, 436);
+            this.DataGridView.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(63)))), ((int)(((byte)(86)))));
+            this.panel4.Controls.Add(this.YLuoButton);
+            this.panel4.Controls.Add(this.YRooliTextBox);
+            this.panel4.Controls.Add(this.YYritysTextBox);
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Controls.Add(this.YNimiTextBox);
+            this.panel4.Controls.Add(this.YSpostiTextBox);
+            this.panel4.Controls.Add(this.YPuhelinTextBox);
+            this.panel4.Location = new System.Drawing.Point(504, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(480, 161);
+            this.panel4.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(63)))), ((int)(((byte)(86)))));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(185, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Yhteyshenkilö";
+            // 
+            // YNimiTextBox
+            // 
+            this.YNimiTextBox.Location = new System.Drawing.Point(30, 33);
+            this.YNimiTextBox.Name = "YNimiTextBox";
+            this.YNimiTextBox.Size = new System.Drawing.Size(216, 26);
+            this.YNimiTextBox.TabIndex = 1;
+            this.YNimiTextBox.Text = "Nimi";
+            // 
+            // YSpostiTextBox
+            // 
+            this.YSpostiTextBox.Location = new System.Drawing.Point(30, 65);
+            this.YSpostiTextBox.Name = "YSpostiTextBox";
+            this.YSpostiTextBox.Size = new System.Drawing.Size(216, 26);
+            this.YSpostiTextBox.TabIndex = 2;
+            this.YSpostiTextBox.Text = "Sähköposti";
+            // 
+            // YPuhelinTextBox
+            // 
+            this.YPuhelinTextBox.Location = new System.Drawing.Point(30, 96);
+            this.YPuhelinTextBox.Name = "YPuhelinTextBox";
+            this.YPuhelinTextBox.Size = new System.Drawing.Size(216, 26);
+            this.YPuhelinTextBox.TabIndex = 3;
+            this.YPuhelinTextBox.Text = "Puhelinnumero";
+            // 
+            // YYritysTextBox
+            // 
+            this.YYritysTextBox.Location = new System.Drawing.Point(252, 33);
+            this.YYritysTextBox.Name = "YYritysTextBox";
+            this.YYritysTextBox.Size = new System.Drawing.Size(216, 26);
+            this.YYritysTextBox.TabIndex = 5;
+            this.YYritysTextBox.Text = "Yritys";
+            // 
+            // YRooliTextBox
+            // 
+            this.YRooliTextBox.Location = new System.Drawing.Point(252, 65);
+            this.YRooliTextBox.Name = "YRooliTextBox";
+            this.YRooliTextBox.Size = new System.Drawing.Size(216, 26);
+            this.YRooliTextBox.TabIndex = 6;
+            this.YRooliTextBox.Text = "Rooli";
+            // 
+            // YLuoButton
+            // 
+            this.YLuoButton.Location = new System.Drawing.Point(252, 96);
+            this.YLuoButton.Name = "YLuoButton";
+            this.YLuoButton.Size = new System.Drawing.Size(102, 27);
+            this.YLuoButton.TabIndex = 7;
+            this.YLuoButton.Text = "Luo";
+            this.YLuoButton.UseVisualStyleBackColor = true;
+            // 
+            // ALuoButton
+            // 
+            this.ALuoButton.Location = new System.Drawing.Point(234, 129);
+            this.ALuoButton.Name = "ALuoButton";
+            this.ALuoButton.Size = new System.Drawing.Size(102, 27);
+            this.ALuoButton.TabIndex = 4;
+            this.ALuoButton.Text = "Luo";
+            this.ALuoButton.UseVisualStyleBackColor = true;
+            // 
+            // YtunnusTextBox
+            // 
+            this.YtunnusTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.YtunnusTextBox.Location = new System.Drawing.Point(12, 65);
+            this.YtunnusTextBox.Name = "YtunnusTextBox";
+            this.YtunnusTextBox.Size = new System.Drawing.Size(216, 26);
+            this.YtunnusTextBox.TabIndex = 5;
+            this.YtunnusTextBox.Text = "Y-tunnus";
+            // 
+            // KatuosoiteTextBox
+            // 
+            this.KatuosoiteTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.KatuosoiteTextBox.Location = new System.Drawing.Point(12, 97);
+            this.KatuosoiteTextBox.Name = "KatuosoiteTextBox";
+            this.KatuosoiteTextBox.Size = new System.Drawing.Size(216, 26);
+            this.KatuosoiteTextBox.TabIndex = 6;
+            this.KatuosoiteTextBox.Text = "Katuosoite";
+            // 
+            // ToimialaTextBox
+            // 
+            this.ToimialaTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.ToimialaTextBox.Location = new System.Drawing.Point(234, 97);
+            this.ToimialaTextBox.Name = "ToimialaTextBox";
+            this.ToimialaTextBox.Size = new System.Drawing.Size(216, 26);
+            this.ToimialaTextBox.TabIndex = 7;
+            this.ToimialaTextBox.Text = "Toimiala";
+            // 
+            // YritysnimiTextBox
+            // 
+            this.YritysnimiTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.YritysnimiTextBox.Location = new System.Drawing.Point(12, 33);
+            this.YritysnimiTextBox.Name = "YritysnimiTextBox";
+            this.YritysnimiTextBox.Size = new System.Drawing.Size(216, 26);
+            this.YritysnimiTextBox.TabIndex = 8;
+            this.YritysnimiTextBox.Text = "Yrityksen nimi";
+            // 
+            // KaupunkiTextBox
+            // 
+            this.KaupunkiTextBox.Location = new System.Drawing.Point(234, 65);
+            this.KaupunkiTextBox.Name = "KaupunkiTextBox";
+            this.KaupunkiTextBox.Size = new System.Drawing.Size(216, 26);
+            this.KaupunkiTextBox.TabIndex = 9;
+            this.KaupunkiTextBox.Text = "Kaupunki";
+            // 
+            // APuhelinTextBox
+            // 
+            this.APuhelinTextBox.Location = new System.Drawing.Point(12, 129);
+            this.APuhelinTextBox.Name = "APuhelinTextBox";
+            this.APuhelinTextBox.Size = new System.Drawing.Size(216, 26);
+            this.APuhelinTextBox.TabIndex = 10;
+            this.APuhelinTextBox.Text = "Puhelinnumero";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(947, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 25);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox HakuTextBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button PoistaButton;
+        private System.Windows.Forms.Button MuokkaaButton;
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PostinumeroTextBox;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button YLuoButton;
+        private System.Windows.Forms.TextBox YRooliTextBox;
+        private System.Windows.Forms.TextBox YYritysTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox YNimiTextBox;
+        private System.Windows.Forms.TextBox YSpostiTextBox;
+        private System.Windows.Forms.TextBox YPuhelinTextBox;
+        private System.Windows.Forms.Button ALuoButton;
+        private System.Windows.Forms.TextBox APuhelinTextBox;
+        private System.Windows.Forms.TextBox KaupunkiTextBox;
+        private System.Windows.Forms.TextBox YritysnimiTextBox;
+        private System.Windows.Forms.TextBox ToimialaTextBox;
+        private System.Windows.Forms.TextBox KatuosoiteTextBox;
+        private System.Windows.Forms.TextBox YtunnusTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
